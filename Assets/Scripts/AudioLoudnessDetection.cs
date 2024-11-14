e05ecec19e55396ab985080eca36c795228c4eb9
Abdulhafith Ahmed <2711914a@student.gla.ac.uk>
2024-11-14 15:51:14 +0000

    on and off mic button

--- Assets/Scripts/AudioLoudnessDetection.cs
+++ Assets/Scripts/AudioLoudnessDetection.cs
@@ -5,52 +5,84 @@ using UnityEngine;
 public class AudioLoudnessDetection : MonoBehaviour
 {
     public int sampleWindow = 64;
+    public bool isMicrophoneActive = true; // Toggle microphone detection
     private AudioClip microphoneClip;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        MicrophoneToAudioClip();
+        if (isMicrophoneActive)
+        {
+            MicrophoneToAudioClip();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isMicrophoneActive && microphoneClip == null)
+        {
+            MicrophoneToAudioClip();
+        }
+        else if (!isMicrophoneActive && microphoneClip != null)
+        {
+            StopMicrophone();
+        }
     }
 
     public void MicrophoneToAudioClip()
     {
-        // get first device input list
-        string microphoneName = Microphone.devices[0];
-        microphoneClip = Microphone.Start(microphoneName, true, 20, AudioSettings.outputSampleRate);
+        // Get the first device from the input list
+        if (Microphone.devices.Length > 0)
+        {
+            string microphoneName = Microphone.devices[0];
+            microphoneClip = Microphone.Start(microphoneName, true, 20, AudioSettings.outputSampleRate);
+        }
+        else
+        {
+            Debug.LogWarning("No microphone detected!");
+        }
     }
 
     public float GetLoudnessFromMicrophone()
     {
-        return GetLoudnessFromAudioClip(Microphone.GetPosition(Microphone.devices[0]), microphoneClip);
-    }
-    
-    public float GetLoudnessFromAudioClip(int clipPosition, AudioClip clip)
-    {
-        int startPosition = clipPosition - sampleWindow;
-        
-        if (startPosition < 0)
+        if (!isMicrophoneActive || microphoneClip == null)
         {
             return 0;
         }
-        
-        float [] waveData = new float[sampleWindow];
-        clip.GetData(waveData, startPosition);
-        
-        // compute loudness
-        float totalLoudness = 0;
 
+        return GetLoudnessFromAudioClip(Microphone.GetPosition(Microphone.devices[0]), microphoneClip);
+    }
+
+    public float GetLoudnessFromAudioClip(int clipPosition, AudioClip clip)
+    {
+        int startPosition = clipPosition - sampleWindow;
+
+        if (startPosition < 0 || clip == null)
+        {
+            return 0;
+        }
+
+        float[] waveData = new float[sampleWindow];
+        clip.GetData(waveData, startPosition);
+
+        // Compute loudness
+        float totalLoudness = 0;
         for (int i = 0; i < sampleWindow; i++)
         {
             totalLoudness += Mathf.Abs(waveData[i]);
         }
-        
+
         return totalLoudness / sampleWindow;
+    }
+
+    public void StopMicrophone()
+    {
+        if (Microphone.IsRecording(null))
+        {
+            Microphone.End(null);
+        }
+
+        microphoneClip = null;
     }
 }
