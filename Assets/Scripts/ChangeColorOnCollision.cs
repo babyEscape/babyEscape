137995bec49193e5e462c5f1a858115034676a9e
Abdulhafith Ahmed <2711914a@student.gla.ac.uk>
2024-11-15 15:28:44 +0000

    Basic physics for turning on a button via object collision

---
+++ Assets/Scripts/ChangeColorOnCollision.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorOnCollision : MonoBehaviour
+{
+    private Renderer cubeRenderer;
+
+    void Start()
+    {
+        // Get the Renderer component to change the color later
+        cubeRenderer = GetComponent<Renderer>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Check if the colliding object is the sphere
+        if (collision.gameObject.CompareTag("Sphere"))
+        {
+            // Change the cube's color to a random color
+            cubeRenderer.material.color = new Color(
+                Random.Range(0f, 1f), 
+                Random.Range(0f, 1f), 
+                Random.Range(0f, 1f)
+            );
+        }
+    }
+}
+
