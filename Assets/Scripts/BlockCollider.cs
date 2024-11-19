f596e978e32b32fc3554d9e900b3ac2cf88c1a36
Cian Choo (student) <2620317C@student.gla.ac.uk>
2024-11-19 15:37:00 +0000

    Block Puzzle logic working

---
+++ Assets/Scripts/BlockCollider.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockCollider : MonoBehaviour
+{
+    public GameObject cribDoor;
+    public GameObject cube;
+    public GameObject cylinder;
+    public GameObject triangle;
+
+    private bool cubeIn;
+    private bool cylinderIn;
+    private bool triangleIn;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubeIn = false;
+        cylinderIn = false;
+        triangleIn = true; // set this to false later when we have a triangle model
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cubeIn && cylinderIn && triangleIn)
+        { 
+            cribDoor.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject == cube)
+        {
+            cubeIn = true;
+        }
+        else if (other.gameObject == cylinder)
+        {
+            cylinderIn = true;
+        }
+        // else if (other.gameObject == triangle)
+        // {
+        //     triangleIn = true;
+        // }
+    }
+}
