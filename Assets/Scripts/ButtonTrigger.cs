531150ef81d2f44a617e5cb58178aa57e2aafac2
Nour Elfangary <noureldinesamehelfangary@gmail.com>
2024-11-27 21:57:39 +0000

    Added tunelling vignette on fall Delayed crate launch Adjusted UI look Made UI appear closer to not collide with world objects

--- Assets/Scripts/ButtonTrigger.cs
+++ Assets/Scripts/ButtonTrigger.cs
@@ -4,24 +4,31 @@ using UnityEngine;
 
 public class ButtonTrigger : MonoBehaviour
 {
-    public GameObject cube; // Assign the cube GameObject in the Inspector
-    public float forceMagnitude = 10f; // Adjust the force as needed
+    public GameObject crate; // Assign the cube GameObject in the Inspector
+    public float forceMagnitude = 250f; // Adjust the force as needed
+    public float delay = 3;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Dummy"))
         {
-            Rigidbody cubeRb = cube.GetComponent<Rigidbody>();
-            if (cubeRb != null)
-            {
-                cubeRb.useGravity = true; // Enable gravity
-                Vector3 forceDirection = Vector3.left;
-                cubeRb.AddForce(forceDirection * forceMagnitude, ForceMode.Impulse);
-            }
-            else
-            {
-                Debug.LogWarning("No Rigidbody found on the cube.");
-            }
+            // play sound here
+            Invoke("LaunchCrate", delay);
+        }
+    }
+
+    private void LaunchCrate()
+    {
+        Rigidbody rb = crate.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.useGravity = true; // Enable gravity
+            Vector3 forceDirection = Vector3.left;
+            rb.AddForce(forceDirection * forceMagnitude, ForceMode.Impulse);
+        }
+        else
+        {
+            Debug.LogWarning("No Rigidbody found on the crate.");
         }
     }
 }
